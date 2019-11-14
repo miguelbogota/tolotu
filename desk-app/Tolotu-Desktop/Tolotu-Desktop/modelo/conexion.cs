b4a56532355c6e70aaa3495422db10755054e01f
juan941223 <57539535+juan941223@users.noreply.github.com>
2019-11-14 00:02:08 -0500

    login + conexion a BD
    interfaz de login, carpetas (modelo, vista controlador) y clase de conexion

---
+++ desk-app/Tolotu-Desktop/Tolotu-Desktop/modelo/conexion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Tolotu_Desktop.modelo
+{
+    class conexion
+    {
+private SqlConnection conecta = new SqlConnection("Server=(local); Database=x; Integrated Security=true");
+        public conexion(){
+
+        }
+
+        public SqlConnection abrirConx(){
+            try{
+                if(this.conecta.State==ConnectionState.Closed){
+                    conecta.Open();
+                    return conecta;
+                }else{
+                    return null;
+                }
+            }
+            catch(SqlException e){
+                MessageBox.Show("no se pudo abrir la conexión" + e);
+                return null;
+            }
+            catch(Exception x){
+                MessageBox.Show("no se puede ejecutar la conexión con el servidor" + x);
+                return null;
+            }
+        }
+        
+            public SqlConnection cerrarConx(){
+                try{
+                    if(this.conecta.State==ConnectionState.Open){
+                        conecta.Close();             
+                        return conecta;    
+                    }else{
+                        return null;
+                    }
+                }
+                catch(SqlException e){
+                    MessageBox.Show("no se pudo cerrar la conexión" + e);
+                    return null;
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show("no se puede ejecutar la conexión con el servidor" + x);
+                    return null;
+                }
+            }
+        }
+    }
+
---
+++ desk-app/Tolotu-Desktop/Tolotu-Desktop/vista/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tolotu_Desktop.controlador;
+
+namespace Tolotu_Desktop
+{
+    // Estado: Activo
+    // Creado por Juan Miguel Castro rojas - 13.11.2019
+    // Version 0.1
+    
+    public partial class Form1 : Form
+    {
+        controlador.contLogin contLog = new controlador.contLogin();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Creado por Juan Miguel Castro rojas - 13.11.2019
+        // Version 0.1
+        private void txtUsuario_MouseClick(object sender, MouseEventArgs e)
+        {
+            //evento para visualizar mensages predeterminados cuando el usuario no ha introducido un valor
+            //en el campo de texto
+            if (txtUsuario.Text == "Ingresa tu usuario")
+            {
+                txtUsuario.Text = "";
+            }
+            if (txtContraseña.Text == "")
+            {
+                txtContraseña.Text = "Ingresa tu contraseña";
+            }
+        }
+        // Creado por Juan Miguel Castro rojas - 13.11.2019
+        // Version 0.1
+        private void txtContraseña_MouseClick(object sender, MouseEventArgs e)
+        {
+            //evento para visualizar mensages predeterminados cuando el usuario no ha introducido un valor
+            //en el campo de texto
+            if (txtContraseña.Text == "Ingresa tu contraseña")
+            {
+                txtContraseña.Text = "";
+            }
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "Ingresa tu usuario";
+            }
+        }
+        // Creado por Juan Miguel Castro rojas - 13.11.2019
+        // Version 0.1
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+            contLog.entradaDatos(txtUsuario.Text, txtContraseña.Text);
+        }
+    }
+}
